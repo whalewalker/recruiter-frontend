502829ad5d3d4cba201a3f2139d2181fd9a4d70d
whalewalker <mongodbms@gmail.com>
2021-05-18 17:15:20 +0100

    configure project plugins and dependencies

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React from 'react'
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <h1>Hello wordl</h1>
+      <h1>Hello world</h1>
     </div>
-  )
+  );
 }
 
-export default App
-
-
+export default App;
